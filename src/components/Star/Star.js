0804b92c68703c5ddbc0edf6ad9d7fb65f711929
Curtis Morice <curtismorice@gmail.com>
2018-06-20 10:05:53 -0500

    more lecture and currying

--- src/components/Star/Star.js
+++ src/components/Star/Star.js
@@ -11,7 +11,11 @@ class Star extends Component {
     // Setting state with an = is something you only 
     // want to do in the constructor. Outside of the
     // constructor always use the setState method
-    this.state = { name: 'Menkar', brightness: 1.6 } 
+    this.state = { star: 
+      { name:'',
+        brightness: '', 
+      color: ''} 
+    };
 
     // Make sure that the handleChange knows about 'this'
     // You ONLY need this if you don't make an arrow function
@@ -20,27 +24,43 @@ class Star extends Component {
 
   // By making this an arrow function, 'this' is bound for you!
   // Use arrow functions!  
-  handleChangeForName = (event) => {
-    console.log(event.target.value);
-    // Change state using setState not an = 
-    this.setState( { name: event.target.value } );
-  }
+  // handleChangeForName = (event) => {
+  //   console.log(event.target.value);
+  //   // Change state using setState not an = 
+  //   this.setState( { name: event.target.value } );
+  // }
 
-  handleChangeForBrightness = (event) => {
-    console.log(event.target.value);
-    // Change state using setState not an = 
-    this.setState( { brightness: event.target.value } );
-  }
+  // handleChangeForBrightness = (event) => {
+  //   console.log(event.target.value);
+  //   // Change state using setState not an = 
+  //   this.setState( { brightness: event.target.value } );
+  // }
+  // handleChangeForColor = (event) => {
+  //   console.log(event.target.value);
+  //   // Change state using setState not an = 
+  //   this.setState( { color: event.target.value } );
+  // }
+handleChangeFor =(property) => (event) => {
+  this.setState( {star: {
+  ...this.state.star,
+  [property]: event.target.value
+}
+});
+}
 
   render() {
     return (
       <div>
-        <h2>The Best Star Ever</h2>
-        <input onChange={this.handleChangeForName} />
-        <input onChange={this.handleChangeForBrightness} />
-        <p>Name: {this.state.name}</p>
-        <p>Brightness: {this.state.brightness}</p>
+        <h2>The Star</h2>
+        <input onChange={this.handleChangeFor('name')} />
+        <input onChange={this.handleChangeFor('brightness')} />
+        <input onChange={this.handleChangeFor('color')} />
+        <p>Name: {this.state.star.name}</p>
+        <p>Brightness: {this.state.star.brightness}</p>
+        <p>Color: {this.state.star.color}</p>
+    
       </div>
+      
     );
   }
 }
